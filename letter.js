85a9f0cc70224cf400e7f6b6e86dbe32d9b252df
An Phan <anphan@Ans-MacBook-Pro.local>
2019-12-07 09:55:27 -0600

    brandon

---
+++ letter.js
@@ -0,0 +1,27 @@
+//NOTE: created Letter constructor for individual letters
+
+const Letter = function (character) {
+
+    this.character = character;
+    this.guess = false;
+    this.display = function () {
+        if (this.guess === true) {
+            return this.character;
+        } else {
+            return "_";
+        }
+
+    };
+
+    this.guessCheck = function (userGuess) {
+        if (userGuess === this.character) {
+            this.guess = true;
+        } else {
+            this.guess = false;
+        }
+    };
+
+
+}
+
+//hi
